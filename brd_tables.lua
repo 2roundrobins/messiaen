9da2f796003081618a669dd9800bdc7eb0e106f7
2roundrobins <139110506+2roundrobins@users.noreply.github.com>
2023-08-31 12:23:42 +0200

    Create brd_tables.lua

---
+++ brd_tables.lua
@@ -0,0 +1,228 @@
+--NIGHTING GALE
+gale_as1 = {
+    {rate = 1, duration = 0.20},--eight
+    {rate = 12, duration = 0.83},--half  
+    {rate = 7, duration = 0.10}, --sixteen  
+    {rate = 8, duration = 0.20}, 
+    {rate = 1, duration = 0.20},
+    {rate = 12, duration =  0.83},
+    {rate = 7, duration = 0.10},
+    {rate = 8, duration = 0.10},
+    {rate = 10, duration = 0.83},
+    {rate = (1)/2, duration = 0.20}
+  }
+  
+  --EUROPEAN ROBIN
+  robin_as1 = {
+    {rate = 19, duration = 0.105},
+    {rate = 17, duration = 0.105},
+    {rate = 14, duration = 0.105},
+    {rate = 12, duration = 0.105},
+    {rate = 9, duration = 0.105},
+    {rate = 7, duration = 0.105},
+    {rate = 5, duration = 0.105},
+    {rate = 2, duration = 0.105},
+    {rate = 1, duration = 0.215},
+  }
+  robin_as2 = {
+    {rate = 14, duration = 0.105},
+    {rate = 12, duration = 0.435},
+  }
+  robin_as3 = {
+    {rate = 1, duration = 0.215},
+  }
+  
+  --EUROASIAN WREN
+  wren_as1 = {
+    {rate = 6, duration = 0.18},   
+    {rate = 3, duration = 0.18},   
+    {rate = 1, duration = 0.18},   
+    {rate = 11, duration = 0.36}, }
+  wren_as2 = {
+    {rate = 3, duration = 0.18},
+    {rate = 1, duration = 0.18},
+    {rate = 11, duration = 0.36},
+    {rate = 1, duration = 0.18},
+    {rate = 2, duration = 0.004},
+    {rate = 1, duration = 0.09}, }
+  wren_as3 = {  
+    {rate = 1.5, duration = 0.09},
+    {rate = 1, duration = 0.09},
+    {rate = 1.5, duration = 0.09},
+    {rate = 1, duration = 0.09},
+    {rate = 1.5, duration = 0.09},
+    {rate = 1, duration = 0.09},
+    {rate = 1.5, duration = 0.09},
+    {rate = 1, duration = 0.09},
+    {rate = 1.5, duration = 0.09},
+    {rate = 1, duration = 0.09},
+    {rate = 1.5, duration = 0.09},
+    {rate = 1, duration = 0.09},
+    {rate = 1.5, duration = 0.09},
+    {rate = 1, duration = 0.09},
+    {rate = 1.5, duration = 0.09},
+    {rate = 5, duration = 0.36},
+    {rate = 6, duration = 0.18},}
+  wren_as4 ={  
+    {rate = 11, duration = 0.18},
+    {rate = 1, duration = 0.18}, 
+    {rate = 11, duration = 0.18},
+    {rate = 1, duration = 0.18}, 
+    {rate = 11, duration = 0.18},
+    {rate = 1, duration = 0.18}, 
+    {rate = 11, duration = 0.18},
+    {rate = 1, duration = 0.18}, 
+    {rate = 11, duration = 0.18},
+    {rate = 1, duration = 0.18}, 
+    {rate = 11, duration = 0.18},
+    {rate = 1, duration = 0.36},
+    {rate = (1)/2, duration = 0.36}
+  }
+  --SONG TRUSH
+  trush_as1 = {
+    {rate = 10, duration = 0.05}, --grace note
+    {rate = 4, duration = 0.1},  --sixteenth
+    {rate = 13, duration = 0.05},
+    {rate = 7, duration = 0.1},
+    {rate = 15, duration = 0.1},
+  }
+  trush_as2 = {
+    {rate = 10, duration = 0.05}, --grace note
+    {rate = 4, duration = 0.1},  --sixteenth
+    {rate = 13, duration = 0.05},
+    {rate = 7, duration = 0.1},
+    {rate = 15, duration = 0.1},
+  }
+  trush_as3 = {
+    {rate = 7, duration = 0.05},  --32th
+    {rate = 8, duration = 0.05},
+    {rate = 14, duration = 0.05},
+    {rate = 13, duration = 0.05},
+    {rate = 12, duration = 0.05},
+    {rate = 10, duration = 0.05},
+    {rate = 9, duration = 0.1},
+  }
+  trush_as4 = {
+    {rate = 1, duration = 0.05},  --32th
+    {rate = 7, duration = 0.05},
+    {rate = 13, duration = 0.05},
+    {rate = 14, duration = 0.05},
+    {rate = 14, duration = 0.1},
+    {rate = 1, duration = 0.05},  --32th
+    {rate = 7, duration = 0.05},
+    {rate = 13, duration = 0.05},
+    {rate = 14, duration = 0.05},
+    {rate = 14, duration = 0.1},
+  }
+  trush_as5 ={
+    {rate = 1, duration = 0.1}
+  }
+  
+  --COMMON REDSTART
+  redstart_as1 = {
+    {rate = 15, duration = 0.17},--eight
+    {rate = 11, duration = 0.09},--sixtenth
+    {rate = 9, duration = 0.17} }
+  redstart_as2 = {  
+    {rate = 12, duration = 0.04},
+    {rate = 10, duration = 0.04},
+    {rate = 3, duration = 0.04},
+    {rate = 8, duration = 0.04},
+    {rate = 13, duration = 0.04},
+    {rate = 2, duration = 0.04},
+    {rate = 1, duration = 0.04},
+    {rate = 8, duration = 0.09} 
+  }
+  redstart_as3 = {  
+    {rate = 1, duration = 0.04}}
+    
+  --BLACKBIRD
+  blackbird_as1 = {
+    {rate = (11) + 12 , duration = 0.052},--32
+    {rate = (11) + 12 , duration = 0.052},
+    {rate = (11) + 12 , duration = 0.052},
+    {rate = 4, duration = 0.052},
+    {rate = (9) + 12 , duration = 0.052},
+    {rate = (11) + 12 , duration = 0.052},
+    {rate = 3, duration = 0.052},
+    {rate = (9) + 12 , duration = 0.052},
+    {rate = (13) + 12 , duration = 0.052},
+    {rate = (10) + 12 , duration = 0.052},
+    {rate = 1 , duration = 0.052},}
+  blackbird_as2 = {  
+    {rate = 1 , duration = 0.208},--eight
+    {rate = (15) + 12 , duration = 0.052},--eight
+    {rate = (17) + 12 , duration = 0.052},
+    {rate = (13) + 12 , duration = 0.104}
+  }
+  blackbird_as3 = {  
+    {rate = 1 , duration = 0.208}
+  }--eight
+  
+  --AWESOME BIRD
+  -- should behave based on scale
+  awesome_1 = {
+      {rate = 1, duration = 1/8},
+      {rate = 7, duration = 1/16},   
+      {rate = 12, duration = 1/8},   
+      {rate = 1, duration = 1/8}, 
+      {rate = 12, duration = 1/4},
+    }
+    awesome_2 = {
+      {rate = 7, duration = 1/8},
+      {rate = 1, duration = 1/16},   
+      {rate = 12, duration = 1/8},   
+      {rate = 4, duration = 1/8}, 
+      {rate = 1, duration = 1/4}
+    }
+    awesome_3 = {
+      {rate = 7, duration = 1/32},
+      {rate = 1, duration = 1/16},   
+      {rate = 7, duration = 1/32},   
+      {rate = 12, duration = 1/8}, 
+      {rate = 1, duration = 1/4},
+      {rate = 7, duration = 1/2},
+      {rate = 12, duration = 1/16},
+      {rate = 4, duration = 1/16},
+      {rate = 7, duration = 1/24},
+      {rate = 12, duration = 1/32},
+      {rate = 4, duration = 1/8},
+      {rate = 1, duration = 1/4},
+    }
+    awesome_4 = {
+      {rate = 1, duration = 1/32},
+      {rate = 2, duration = 1/32},   
+      {rate = 4, duration = 1/32},   
+      {rate = 5, duration = 1/32}, 
+      {rate = 7, duration = 1/32},
+      {rate = 9, duration = 1/32},
+      {rate = 11, duration = 1/32},
+      {rate = 12, duration = 1/32},
+      {rate = 1, duration = 1/4},
+      {rate = 12, duration = 1/32},
+      {rate = 11, duration = 1/32},
+      {rate = 9, duration = 1/32},
+      {rate = 7, duration = 1/16},
+      {rate = 5, duration = 1/8}, 
+      {rate = 4, duration = 1/4},
+      {rate = 2, duration = 1/2}, 
+      {rate = 1, duration = 1/1},
+    }
+    awesome_5 = {
+      {rate = 1, duration = 1/8},
+      {rate = 7, duration = 1/16},   
+      {rate = 1, duration = 1/8},   
+      {rate = 7, duration = 1/8}, 
+      {rate = 12, duration = 1/4}
+    }
+    
+    --WEIRD BIRD
+    -- should behave based on scale
+    weird = {
+      {rate = math.random((12)+2 / 2), duration = 1/4},
+      {rate = math.random((24)+2 / 2), duration = 1/4},   
+      {rate = math.random((7)+3*2), duration = 1/4},   
+      {rate = math.random((24)+2 / 2), duration = 1/4}, 
+      {rate = math.random((12)+2 / 2), duration = 1/4},
+    }
+    
