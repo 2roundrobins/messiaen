c87c30d86f382e8e2cf1dff35ef5bf743ecd62a3
sonocircuit <49308219+sonocircuit@users.noreply.github.com>
2023-10-08 22:38:20 +0200

    bird table now works
    @2roundrobins few things to optimise here. we still need a way to clearly assign the params and sc voice to the active birds.

--- lib/birds.lua
+++ lib/birds.lua
@@ -1,26 +1,32 @@
-function play_birdsongs(bird)
-
-    local songcount = 0
-    while true do
-      softcut.level(1, volume)
-      local song = math.random(1, #bird)
-      -- play notes
-      for i = 1, #bird[song] do
-        local current_note = bird[song][i]
-        local rate = ntor(current_note.rate) * 3
-        softcut.rate(1, rate)
-        clock.sleep(current_note.duration)
-      end
-      softcut.level(1, 0)
-      clock.sleep(math.random(1) + 0.2)
-    end
-  end
-
-
 bird = {}
+
+-- bird table
+bird.names = {"wren", "robin", "trush", "nightingale", "blackbird", "finch", "great tit"}
+
+bird.voice = {}
+for i = 1, 5 do
+  bird.voice[i] = {}
+  bird.voice[i].sc = i
+  bird.voice[i].name = ""
+  bird.voice[i].level = 0
+  bird.voice[i].pan = 0
+  bird.voice[i].cutoff = 18000
+  bird.voice[i].filter_q = 4
+
+end
+
+bird.params = {}
+for i = 1, 5 do
+  bird.params[i] = {}
+  bird.params[i].level = 0
+  bird.params[i].pan = 0
+  bird.params[i].cutoff = 18000
+  bird.params[i].filter_q = 4
+end
 
 --wren
 bird.wren = {}
+bird.wren.name = "wren"
 for i = 1, 4 do
   bird.wren[i] = {}
 end
@@ -29,14 +35,16 @@ bird.wren[1] = {
     {r = 6, d = 0.18},   
     {r = 3, d = 0.18},   
     {r = 1, d = 0.18},   
-    {r = 11, d = 0.36}, }
+    {r = 11, d = 0.36}
+  }
 bird.wren[2] = {
     {r = 3, d = 0.18},
     {r = 1, d = 0.18},
     {r = 11, d = 0.36},
     {r = 1, d = 0.18},
     {r = 2, d = 0.004},
-    {r = 1, d = 0.09}, }
+    {r = 1, d = 0.09}
+  }
 bird.wren[3] = {  
     {r = 1.5, d = 0.09},
     {r = 1, d = 0.09},
@@ -54,7 +62,8 @@ bird.wren[3] = {
     {r = 1, d = 0.09},
     {r = 1.5, d = 0.09},
     {r = 5, d = 0.36},
-    {r = 6, d = 0.18},}
+    {r = 6, d = 0.18}
+  }
 bird.wren[4]={  
     {r = 11, d = 0.18},
     {r = 1, d = 0.18}, 
@@ -68,11 +77,12 @@ bird.wren[4]={
     {r = 1, d = 0.18}, 
     {r = 11, d = 0.18},
     {r = 1, d = 0.36},
-    {r = (1)/2, d = 0.36}
+    {r = 1/2, d = 0.36}
   }
 
 --european robin
 bird.robin = {}
+bird.robin.name = "robin"
 for i = 1, 3 do
   bird.robin[i] = {}
 end
@@ -86,35 +96,36 @@ bird.robin[1] = {
     {r = 7, d = 0.105},
     {r = 5, d = 0.105},
     {r = 2, d = 0.105},
-    {r = 1, d = 0.215},
+    {r = 1, d = 0.215}
   }
 bird.robin[2] = {
     {r = 14, d = 0.105},
-    {r = 12, d = 0.435},
+    {r = 12, d = 0.435}
   }
 bird.robin[3] = {
-    {r = 1, d = 0.215},
+    {r = 1, d = 0.215}
   }
 
 --song trush
 bird.trush = {}
+bird.trush.name = "trush"
 for i = 1, 5 do
   bird.trush[i] = {}
 end
  
 bird.trush[1] = {
-    {r = 10, d = 0.05}
+    {r = 10, d = 0.05},
     {r = 4, d = 0.1},  
     {r = 13, d = 0.05},
     {r = 7, d = 0.1},
-    {r = 15, d = 0.1},
+    {r = 15, d = 0.1}
   }
 bird.trush[2] = {
     {r = 10, d = 0.05}, 
     {r = 4, d = 0.1}, 
     {r = 13, d = 0.05},
     {r = 7, d = 0.1},
-    {r = 15, d = 0.1},
+    {r = 15, d = 0.1}
   }
 bird.trush[3] = {
     {r = 7, d = 0.05},
@@ -123,7 +134,7 @@ bird.trush[3] = {
     {r = 13, d = 0.05},
     {r = 12, d = 0.05},
     {r = 10, d = 0.05},
-    {r = 9, d = 0.1},
+    {r = 9, d = 0.1}
   }
 bird.trush[4]= {
     {r = 1, d = 0.05},
@@ -135,15 +146,15 @@ bird.trush[4]= {
     {r = 7, d = 0.05},
     {r = 13, d = 0.05},
     {r = 14, d = 0.05},
-    {r = 14, d = 0.1},
+    {r = 14, d = 0.1}
   }
 bird.trush[5] ={
     {r = 1, d = 0.1}
   }
   
-
  --nightingale
 bird.gale = {}
+bird.gale.name = "gale"
 for i = 1, 3 do
   bird.gale[i] = {}
 end 
@@ -158,7 +169,7 @@ bird.gale[1] = {
     {r = 7, d = 0.10},
     {r = 8, d = 0.10},
     {r = 10, d = 0.83},
-    {r = (1)/2, d = 0.20}
+    {r = 1/2, d = 0.20}
   }
 
   bird.gale[2] = { 
@@ -171,7 +182,7 @@ bird.gale[1] = {
     {r = 7, d = 0.10},
     {r = 8, d = 0.10},
     {r = 10, d = 0.83},
-    {r = (1)/2, d = 0.20}
+    {r = 1/2, d = 0.20}
   }
 
   bird.gale[3] = { 
@@ -184,7 +195,7 @@ bird.gale[1] = {
     {r = 7, d = 0.10},
     {r = 8, d = 0.10},
     {r = 10, d = 0.83},
-    {r = (1)/2, d = 0.20}
+    {r = 1/2, d = 0.20}
   }
 
 
@@ -195,34 +206,36 @@ for i = 1, 4 do
 end 
 
 bird.blackbird[1] = {
-    {r = (11) + 12 , d = 0.052},
-    {r = (11) + 12 , d = 0.052},
-    {r = (11) + 12 , d = 0.052},
+    {r = 11 + 12 , d = 0.052},
+    {r = 11 + 12 , d = 0.052},
+    {r = 11 + 12 , d = 0.052},
     {r = 4, d = 0.052},
-    {r = (9) + 12 , d = 0.052},
-    {r = (11) + 12 , d = 0.052},
+    {r = 9 + 12 , d = 0.052},
+    {r = 11 + 12 , d = 0.052},
     {r = 3, d = 0.052},
-    {r = (9) + 12 , d = 0.052},
-    {r = (13) + 12 , d = 0.052},
-    {r = (10) + 12 , d = 0.052},
-    {r = 1 , d = 0.052},}
+    {r = 9 + 12 , d = 0.052},
+    {r = 13 + 12 , d = 0.052},
+    {r = 10 + 12 , d = 0.052},
+    {r = 1 , d = 0.052}
+  }
 bird.blackbird[2] = {  
     {r = 1 , d = 0.208},
-    {r = (15) + 12 , d = 0.052},
-    {r = (17) + 12 , d = 0.052},
-    {r = (13) + 12 , d = 0.104}
+    {r = 15 + 12 , d = 0.052},
+    {r = 17 + 12 , d = 0.052},
+    {r = 13 + 12 , d = 0.104}
   }
 bird.blackbird[3] = {  
     {r = 1 , d = 0.208}
   }
 bird.blackbird[4] = {
-      {r = (9) + 12 , d = 0.052, pb = 0},
-      {r = (11) + 12 , d = 0.052, pb = 0.3},
+      {r = 9 + 12 , d = 0.052, pb = 0},
+      {r = 11 + 12 , d = 0.052, pb = 0.3},
       {r = 3, d = 0.052},
-      {r = (9) + 12 , d = 0.052, pb = 0},
-      {r = (13) + 12 , d = 0.052, pb = 0.2},
-      {r = (10) + 12 , d = 0.052, pb = 0},
-      {r = 1 , d = 0.052, pb = 1},}
+      {r = 9 + 12 , d = 0.052, pb = 0},
+      {r = 13 + 12 , d = 0.052, pb = 0.2},
+      {r = 10 + 12 , d = 0.052, pb = 0},
+      {r = 1 , d = 0.052, pb = 1}
+    }
   
 --chafinch
 bird.finch = {}
@@ -260,7 +273,7 @@ bird.finch[1] = {
     {r = 8, d = 0.030},
     {r = 7, d = 0.020},
     {r = 0, d = 0.020},
-    {r = 1.5, d = 0.045},
+    {r = 1.5, d = 0.045}
   }
 
   bird.finch[2] = { -- needs change
@@ -293,7 +306,7 @@ bird.finch[1] = {
     {r = 8, d = 0.030},
     {r = 7, d = 0.020},
     {r = 0, d = 0.020},
-    {r = 1.5, d = 0.045},
+    {r = 1.5, d = 0.045}
   }
 
   bird.finch[3] = { -- needs change
@@ -326,7 +339,7 @@ bird.finch[1] = {
     {r = 8, d = 0.030},
     {r = 7, d = 0.020},
     {r = 0, d = 0.020},
-    {r = 1.5, d = 0.045},
+    {r = 1.5, d = 0.045}
   }
 
 --great tit
@@ -336,46 +349,48 @@ for i = 1, 3 do
 end 
 
 bird.g_tit[1] = {
-  {r = (6) + 12 , d = 0.52},
-  {r = (1) + 12 , d = 0.52},
-  {r = (6) + 12 , d = 0.52},
-  {r = (1.2) + 12 , d = 0.52},
-  {r = (6) + 12 , d = 0.52},
-  {r = (6) + 12 , d = 0.52},
+  {r = 6 + 12 , d = 0.52},
+  {r = 1 + 12 , d = 0.52},
+  {r = 6 + 12 , d = 0.52},
+  {r = 1.2 + 12 , d = 0.52},
+  {r = 6 + 12 , d = 0.52},
+  {r = 6 + 12 , d = 0.52}
 }
+
 bird.g_tit[2] = {
-  {r = (6) + 12 , d = 0.52},
+  {r = 6 + 12 , d = 0.52},
   {r = 0 , d = 0.025}, 
-  {r = (1.1) + 12 , d = 0.52},
+  {r = 1.1 + 12 , d = 0.52},
   {r = 0 , d = 0.025}, 
-  {r = (6) + 12 , d = 0.52},
+  {r = 6 + 12 , d = 0.52},
   {r = 0 , d = 0.025}, 
-  {r = (1.3) + 12 , d = 0.52},
-  {r = 0 , d = 0.025}, 
-  {r = (6.1) + 12 , d = 0.52},
+  {r = 1.3 + 12 , d = 0.52},
+  {r = 0, d = 0.025}, 
+  {r = 6.1 + 12 , d = 0.52},
   {r = 0 , d = 0.25}, 
-  {r = (6) + 12 , d = 0.52},
+  {r = 6 + 12 , d = 0.52}
 }
+
 bird.g_tit[3]= {
-  {r = (6) + 12 , d = 0.25},
-  {r = 0 , d = 0.25}, 
-  {r = (6) + 12 , d = 0.25},
-  {r = 0 , d = 0.025},
-  {r = (9) + 12 , d = 0.25},
-  {r = 0 , d = 0.025}, 
-  {r = (6) + 12 , d = 0.25},
-  {r = 0 , d = 0.025}, 
-  {r = (9) + 12 , d = 0.25},
-  {r = 0 , d = 0.025}, 
-  {r = (6) + 12 , d = 0.25},
-  {r = 0 , d = 0.025}, 
-  {r = (9) + 12 , d = 0.25},
-  {r = 0 , d = 0.025}, 
-  {r = (6) + 12 , d = 0.25},
-  {r = 0 , d = 0.25},
-  {r = (6) + 12 , d = 0.25},
-  {r = 0 , d = 0.025},
-  {r = (9) + 12 , d = 0.25},
+  {r = 6 + 12, d = 0.25},
+  {r = 0, d = 0.25}, 
+  {r = 6 + 12, d = 0.25},
+  {r = 0, d = 0.025},
+  {r = 9 + 12, d = 0.25},
+  {r = 0, d = 0.025}, 
+  {r = 6 + 12, d = 0.25},
+  {r = 0, d = 0.025}, 
+  {r = 9 + 12, d = 0.25},
+  {r = 0, d = 0.025}, 
+  {r = 6 + 1 , d = 0.25},
+  {r = 0, d = 0.025}, 
+  {r = 9 + 12, d = 0.25},
+  {r = 0, d = 0.025}, 
+  {r = 6 + 12, d = 0.25},
+  {r = 0, d = 0.25},
+  {r = 6 + 12, d = 0.25},
+  {r = 0, d = 0.025},
+  {r = 9 + 12, d = 0.25}
 }
 
 ---- special birds ----
@@ -393,7 +408,7 @@ bird.awesome[1] = {
     {r = 7, d = 1/16},   
     {r = 12, d = 1/8},   
     {r = 1, d = 1/8}, 
-    {r = 12, d = 1/4},
+    {r = 12, d = 1/4}
   }
 bird.awesome[2] = {
     {r = 7, d = 1/8},
@@ -414,7 +429,7 @@ bird.awesome[3] = {
     {r = 7, d = 1/24},
     {r = 12, d = 1/32},
     {r = 4, d = 1/8},
-    {r = 1, d = 1/4},
+    {r = 1, d = 1/4}
   }
 bird.awesome[4] = {
     {r = 1, d = 1/32},
@@ -433,7 +448,7 @@ bird.awesome[4] = {
     {r = 5, d = 1/8}, 
     {r = 4, d = 1/4},
     {r = 2, d = 1/2}, 
-    {r = 1, d = 1/1},
+    {r = 1, d = 1/1}
   }
 bird.awesome[5] = {
     {r = 1, d = 1/8},
@@ -454,7 +469,7 @@ bird.weird[1] = {
     {r = math.random((24)+2 / 2), d = 1/4},   
     {r = math.random((7)+3*2), d = 1/4},   
     {r = math.random((24)+2 / 2), d = 1/4}, 
-    {r = math.random((12)+2 / 2), d = 1/4},
+    {r = math.random((12)+2 / 2), d = 1/4}
   }
 
   bird.weird[2] = {
@@ -462,5 +477,9 @@ bird.weird[1] = {
     {r = math.random((24)+2 / 2), d = 1/4},   
     {r = math.random((7)+3*2), d = 1/4},   
     {r = math.random((24)+2 / 2), d = 1/4}, 
-    {r = math.random((12)+2 / 2), d = 1/4},
+    {r = math.random((12)+2 / 2), d = 1/4}
   }
+
+  bird.reference = {}
+
+  return bird
